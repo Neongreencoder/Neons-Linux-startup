06b9972fe99cc23f1e0b03585f37d9f2922ea438
Neongreencoder <43829879+Neongreencoder@users.noreply.github.com>
2021-02-10 10:21:14 -0500

    Version 1

---
+++ Setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:dawidd0811/neofetch
+
+sudo apt update && sudo apt install neofetch
+
+sudo apt install apt-transport-https curl gnupg
+
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt install brave-browser
+
+sudo apt-get update
+
+sudo apt-get upgrade
